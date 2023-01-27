c4fb9a9ab704014d754c040374be9d6a10f4207e
wnduddl30 <leejy3022@naver.com>
2023-01-27 22:01:56 +0900

    LinkedList 추가 메소드 구현

--- src/LinkedList/Test.java
+++ src/LinkedList/Test.java
@@ -3,7 +3,32 @@ package LinkedList;
 import interface_form.LinkedList.SLinkedList;
 
 public class Test {
-    public static void main(String[]args){
-        SLinkedList<Integer> list = new SLinkedList<>(null, null, 0);
-    }
+    public static void main(String[]args) throws CloneNotSupportedException {
+        SLinkedList<Integer> original = new SLinkedList<>(null,null,0);
+        original.add(10);	// original에 10추가
+
+        SLinkedList<Integer> copy = original;
+        SLinkedList<Integer> clone = (SLinkedList<Integer>) original.clone();
+
+        copy.add(20);	// copy에 20추가
+        clone.add(30);	// clone에 30추가
+
+        System.out.println("original list");
+        for(int i = 0; i < original.size(); i++) {
+            System.out.println("index " + i + " data = " + original.get(i));
+        }
+
+        System.out.println("\ncopy list");
+        for(int i = 0; i < copy.size(); i++) {
+            System.out.println("index " + i + " data = " + copy.get(i));
+        }
+
+        System.out.println("\nclone list");
+        for(int i = 0; i < clone.size(); i++) {
+            System.out.println("index " + i + " data = " + clone.get(i));
+        }
+
+        System.out.println("\noriginal list reference : " + original);
+        System.out.println("copy list reference : " + copy);
+        System.out.println("clone list reference : " + clone);    }
 }
--- src/interface_form/LinkedList/SLinkedList.java
+++ src/interface_form/LinkedList/SLinkedList.java
@@ -2,9 +2,12 @@ package interface_form.LinkedList;
 
 import interface_form.ArrayList.List;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.NoSuchElementException;
 
-public class SLinkedList<E> implements List<E> {
+public class SLinkedList<E> implements List<E>,Cloneable {
     private Node<E> head;   //노드의 첫 부분
     private Node<E> tail;   //노드의 마지막 부분
     private int size;       //요소의 개수(=연결된 노드의 개수)
@@ -220,16 +223,29 @@ public class SLinkedList<E> implements List<E> {
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size==0;
     }
 
     @Override
     public void clear() {
+        /**
+         * 그냥 객체 자체를 null 해주기 보다는 모든 노드를 하나하나 null 해주는 것이
+         * 자바의 가비지 컬렉터가 명시적으로 해당 메모리를 안쓴다고 인지하기 떄문에
+         * 메모리 관리 효율 측면에서 조금이나마 더 좋다.
+         */
+        for(Node<E> x = head; x!=null;){
+            Node<E> nextNode = x.next;
+            x.data = null;
+            x.next = null;
+            x = nextNode;
+        }
+        head = tail = null;
+        size = 0;
 
     }
 
@@ -245,5 +261,60 @@ public class SLinkedList<E> implements List<E> {
             x=x.next;           //x노드의 다음 노드를 x에 저장
         }
         return x;
+    }
+
+    public Object clone() throws CloneNotSupportedException{
+        @SuppressWarnings("unchecked")
+        SLinkedList<? super E> clone = (SLinkedList<? super E>)super.clone();
+        clone.head=null;
+        clone.tail=null;
+        clone.size=0;
+
+        for(Node<E> x = head; x !=null; x=x.next){
+            clone.addLast(x.data);
+        }
+
+        return clone;
+    }
+
+    public Object[] toArray() {
+        Object[] array = new Object[size];
+        int idx = 0;
+        for (Node<E> x = head; x !=null; x=x.next){
+            array[idx++] = (E)x.data;
+        }
+        return array;
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T> T[] toArray(T[] a){
+        if (a.length < size){
+            a = (T[])java.lang.reflect.Array.newInstance(a.getClass().getComponentType(), size);
+        }
+        int i = 0;
+        Object[] result = a;
+        for(Node<E> x = head; x!=null; x=x.next) {
+            result[i++] = x.data;
+        }
+        return a;
+    }
+
+    public void sort() {
+        /**
+         * Comparator를 넘겨주지 않은 경우 해당 객체의 Comparable에 구현된 정렬 방식을 사용
+         * 만약 구현되어있지 않으면 cannot be cast to java class java.lang.Comparable 에러가 발생
+         * 만약 구현되어 있을 경우 null로 파라미터를 넘기면 Arrays.sort()가 객체의 CompareTo 메소드에 정의된 방식대로 정렬
+         */
+        sort(null);
+    }
+
+    @SuppressWarnings("unchecked")
+    public void sort(Comparator<? super E> c){
+        Object[] a = this.toArray();
+        Arrays.sort(a, (Comparator)c);
+        int i = 0;
+        for(Node<E> x = head; x!=null; x=x.next){
+            x.data = (E)a[i];
+        }
     }
 }
