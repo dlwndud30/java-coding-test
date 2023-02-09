84827d7cd7907d7bf07dd26c0575a54abe3eedfe
wnduddl30 <leejy3022@naver.com>
2023-02-10 01:31:03 +0900

    연결리스트를 이용한 덱

--- src/interface_form/Queue/linkedDeque/LinkedListDeque.java
+++ src/interface_form/Queue/linkedDeque/LinkedListDeque.java
@@ -170,4 +170,34 @@ public class LinkedListDeque<E> implements Queue<E> {
         }
         return item;
     }
+
+    public int size(){
+        return size;
+    }
+
+    public boolean isEmpty(){
+        return size==0;
+    }
+
+    public boolean contains(Object value){
+        for(Node<E> x =head; x !=null; x=x.next){
+            if(value.equals(x.data)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void clear() {
+        for (Node<E> x = head; x != null;) {
+            Node<E> next = x.next;
+
+            x.data = null;
+            x.next = null;
+            x.prev = null;
+            x = next;
+        }
+        size = 0;
+        head = tail = null;
+    }
 }
