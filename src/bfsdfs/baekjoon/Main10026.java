8eea9909b6b0916fee8ce369b96390a3db2dad14
wnduddl30 <leejy3022@naver.com>
2023-03-15 22:34:09 +0900

    10026-적록색약

---
+++ src/bfsdfs/baekjoon/Main10026.java
@@ -0,0 +1,21 @@
+package bfsdfs.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main10026 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        String map [][] = new String[n][n];
+        for(int i=0; i<n; i++){
+            String string = br.readLine();
+            for(int j=0; j<n; j++){
+                map[i][j] = String.valueOf(string.charAt(j));
+            }
+        }
+    }
+}
