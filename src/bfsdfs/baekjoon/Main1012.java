38867fac413415b6677d3be5cb906f275dc84af4
wnduddl30 <leejy3022@naver.com>
2023-03-20 23:19:01 +0900

    1012 - 유기농 배추

--- src/bfsdfs/baekjoon/Main1012.java
+++ src/bfsdfs/baekjoon/Main1012.java
@@ -1,16 +1,16 @@
 package bfsdfs.baekjoon;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main1012 {
     static int m, n;
     static int [][] map;
+    static int [][] visited;
     static int count = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int t = Integer.parseInt(br.readLine());
 
@@ -23,6 +23,8 @@ public class Main1012 {
             count =0;
 
             map = new int[n][m];
+            visited = new int[n][m];
+
             for(int j=0; j<k; j++){
                 strings = br.readLine().split(" ");
                 int x = Integer.parseInt(strings[0]);
@@ -30,26 +32,37 @@ public class Main1012 {
                 map[y][x]=1;
             }
 
-            for(int a=0; a<n; a++){
-                for(int b=0; b<m; b++){
-                    System.out.print(map[a][b]);
+            for(int x=0; x<n; x++){
+                for(int y=0; y<m; y++){
+                    if(map[x][y]==1 && visited[x][y]==0){
+                        count += dfs(x,y);
+                    }
                 }
-                System.out.println();
             }
-
-            dfs(0);
-            System.out.println(count);
+            bw.write(count+"\n");
         }
+
+        br.close();
+        bw.flush();
+        bw.close();
+
     }
 
-    private static void dfs(int now) {
-        for(int i=0; i<n; i++){
-            if(map[now][i]==1){
-                dfs(i);
-            }else {
-                count++;
-                return;
+    private static int dfs(int x, int y) {
+        int [] add_x ={-1, 0, 1, 0};
+        int [] add_y ={0, 1, 0, -1};
+
+        visited[x][y] = 1;
+
+        for(int i=0; i<4; i++){
+            int xx = x+add_x[i];
+            int yy = y+add_y[i];
+            if(xx>=0 && xx<n && yy>=0 && yy<m){
+                if(map[xx][yy]==1 && visited[xx][yy]==0){
+                    dfs(xx,yy);
+                }
             }
         }
+        return 1;
     }
 }
