5083a956cf0cb4f82aa8233c2a4b6583754594a2
wnduddl30 <leejy3022@naver.com>
2023-03-31 00:36:23 +0900

    9017-크로스 컨트리

---
+++ src/materialization/Main9017.java
@@ -0,0 +1,43 @@
+package materialization;
+
+import java.io.*;
+
+public class Main9017 {
+    public static void main(String [] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<n; i++){
+            int k = Integer.parseInt(br.readLine());
+            String[] inputs = br.readLine().split(" ");
+
+            int [] counts = new int [201];
+            int [] arr = new int [201];
+
+            for(int j=0; j<k; j++){
+                int index = Integer.parseInt(inputs[j]);
+
+                if(counts[index]<4){
+                    counts[index]++;
+                    arr[index] += j+1;
+                }
+
+            }
+
+            int min = Integer.MIN_VALUE;
+            int min_idx = 0;
+            for(int j=1; j<201; j++){
+                if(arr[j]!=0 && counts[j]>=4){
+                    min = Math.min(min, arr[j]);
+                    min_idx = j;
+                }
+            }
+            bw.write(min_idx + "\n");
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
